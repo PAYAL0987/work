6c1fa62914df325e0d52ec1be6200738c4538a79
PAYAL0987 <payalkumari9910@gmail.com>
2025-08-19 19:11:24 +0530

    added file

---
+++ script/day-21.js
@@ -0,0 +1,34 @@
+const userEmail = "payal@.ai"
+
+if(userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+if (userEmail.length ===0) {
+    console.log("Array is empty");
+}
+
+const emptyObj = {}
+if(Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+
+}
+
+// Nullish coalesing opertor(??): null undefine 
+ 
+let val1 ;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+// Terniary  Operator
+
+//conditions ? ture : false 
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80")
---
+++ script/day19.js
@@ -0,0 +1,46 @@
+// control flow  javaScript
+
+
+//const issUeserloggedIn = true
+//const temperture = 41
+//if(temperture < 50) {
+//console.log("temperture is grater than 50");
+
+//} else {
+  //  console.log("payal");
+//}
+
+//console.log("tempuature is greater than 50");
+
+//const score = 200 
+
+//if(score > 100) {
+    //let power = "fly"
+    //console.log('user power: $(power)');
+    
+//}
+//console.log('User power:{power}');
+
+const balance = 1000
+if(balance < 500) {
+    console.log("less than 500");
+}  else if (balance < 750) {
+    console.log("less than 750");
+
+} else {
+    console.log("less than 1220")
+}
+ 
+
+const userLoggedIn = true 
+const debitCard = ture 
+const userLoggedInFormGoggle = false
+const userLoggedInFromEmail = true
+
+if(userLoggedIn && debitCard  && 2==3) {
+    console.log("allow to bye course");
+}
+
+if (userLoggedInform || logggedInFormEamil) {
+    console.log("user logged in");
+}
