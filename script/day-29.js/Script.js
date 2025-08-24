e79e9b7325828e0005ce1a37c87d5770cad7e818
PAYAL0987 <payalkumari9910@gmail.com>
2025-08-24 22:05:34 +0530

    added file

---
+++ script/day-29.js/Script.js
@@ -0,0 +1,11 @@
+const sayDate = function(str) {
+    console.log("payal", Date.now());
+}
+
+
+
+  const intervalId =setInterval(sayDate, 1000, "hi")
+
+  clearInterval = (intervalId
+    
+  )
---
+++ script/day-29.js/app.js
@@ -0,0 +1,17 @@
+const SayPayal = function1() {
+    console.log("Payal");
+}
+
+
+const changeText = function() {
+    document.querySelector('h1').innerHTML = "best js Script"
+}
+
+
+const changeMe =setTimeout(changeText,2000)
+
+document.querySelector(#stop). 
+addEventListener('click', function() {
+    clearTimeout(changeMe)
+    console.log("STOPPED")
+})
